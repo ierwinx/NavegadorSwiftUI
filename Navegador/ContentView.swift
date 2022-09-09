a2e9955d41faf875aa3e8df6fc08a98640e9c539
Eяw!и Gontier <ierwinx@me.com>
2022-09-09 13:44:29 -0500

    Initial Commit

---
+++ Navegador/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Navegador
+//
+//  Created by Erwin Luz León on 09/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navegador/NavegadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavegadorApp.swift
+//  Navegador
+//
+//  Created by Erwin Luz León on 09/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NavegadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
